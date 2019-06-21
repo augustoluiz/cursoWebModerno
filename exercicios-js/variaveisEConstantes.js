569fe51f420e65528f5367c28146719452ea3999
augustoluiz <augustogu7@yahoo.com.br>
2019-06-21 01:25:29 -0300

    Exercícios de Fundamentos - Variáveis e Constantes

---
+++ exercicios-js/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+/** 
+ *Obs: É recomendável o uso do let ao invés do var. O let é mais recente
+*/
+//declaração com var
+var a = 3
+
+//declaração com let
+let b = 4
+
+//declaração com const(constante)
+const c = 5
+
+//o var permite declarar novamente uma variável, o let não. Usando o let, você apenas atribui outro valor
+var a = 30
+b = 40
+
+console.log(a, b)
