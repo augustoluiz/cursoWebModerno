61e8de1529afaa3cf68bcd3c923212d9843f9751
augustoluiz <augustogu7@yahoo.com.br>
2019-06-22 17:34:03 -0300

    Exercícios de Fundamentos - Math

---
+++ exercicios-js/Math.js
@@ -0,0 +1,8 @@
+//Algumas utilizações do Math, bem parecido com o Java
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+console.log(area)
+
+//O Math é um objetc e não uma function
+console.log(typeof Math)
