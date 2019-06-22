f2a327865895893f90bf5d6abf15dbd4cf9cb215
augustoluiz <augustogu7@yahoo.com.br>
2019-06-22 17:10:39 -0300

    Fundamentos - Tipagem Fraca

---
+++ exercicios-js/tipagemFraca.js
@@ -0,0 +1,8 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//mudando o tipo da variável de string para number
+qualquer = 3.15
+console.log(qualquer)
+console.log(typeof qualquer)
