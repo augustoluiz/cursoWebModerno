97cfc87492f381e6fa97b16f8ed405dbccfd0e09
augustoluiz <augustogu7@yahoo.com.br>
2019-06-23 23:56:06 -0300

    Ecercícios de Fundamentos - Boolean parte 1

---
+++ exercicios-js/Boolean.js
@@ -0,0 +1,12 @@
+let isActive = false
+console.log(isActive)
+
+isActive = true
+console.log(isActive)
+
+//colocando o ! gera o 'false'
+isActive = 1
+console.log(!isActive)
+
+//colocando o !! gera o 'true'
+console.log(!!isActive)
