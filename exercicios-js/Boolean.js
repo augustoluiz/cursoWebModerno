3d6b685c68597664dbb8bebf7959045b32fd9546
augustoluiz <augustogu7@yahoo.com.br>
2019-06-24 23:09:24 -0300

    Exercícios de Fundamentos - Boolean parte 2

--- exercicios-js/Boolean.js
+++ exercicios-js/Boolean.js
@@ -10,3 +10,23 @@ console.log(!isActive)
 
 //colocando o !! gera o 'true'
 console.log(!!isActive)
+
+//situações que retornam true
+console.log('Verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!'texto')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isActive = true))
+
+//retornam false
+console.log('Os Falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isActive = false))
