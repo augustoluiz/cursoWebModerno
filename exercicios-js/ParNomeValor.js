df63f4a1eee509438113eae1fe60de2fcecc12ce
augustoluiz <augustogu7@yahoo.com.br>
2019-07-02 23:12:55 -0300

    Exercícios de Fundamentos - Par Nome/Valor

---
+++ exercicios-js/ParNomeValor.js
@@ -0,0 +1,18 @@
+//Par Nome/Valor
+const saudacao = 'Olá, Mundo!'
+
+function exec() {
+    const saudacao = 'Oi'
+    return saudacao
+}
+
+//Objetos são grupos aninhados de nome/valor
+const Cliente = {
+    nome: 'Augusto',
+    endereco: 'Rua 123',
+    telefone: '119999-9999'
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(Cliente)
