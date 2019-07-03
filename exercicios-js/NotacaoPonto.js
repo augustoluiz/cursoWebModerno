82b6fda04633a087312bb44a709cca4e625b082c
augustoluiz <augustogu7@yahoo.com.br>
2019-07-02 23:26:30 -0300

    Exercícios de Fundamentos - Notação Ponto

---
+++ exercicios-js/NotacaoPonto.js
@@ -0,0 +1,16 @@
+console.log(Math.ceil(6,2))
+
+const obj = {}
+obj.nome = 'bola'
+
+console.log(obj.nome)
+
+function Obj(nome){
+    this.nome = nome
+}
+
+Obj('Augusto')
+console.log(nome)
+
+const obj2 = new Obj('Cadeira')
+console.log(obj2.nome)
