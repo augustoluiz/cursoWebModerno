3cbb821fd4660ebf58457bc3ea0e8a7b93755aa4
augustoluiz <augustogu7@yahoo.com.br>
2019-07-02 23:34:38 -0300

    Exercícios de Fundamentos - Operadores de Atribuição

---
+++ exercicios-js/Atribuicao.js
@@ -0,0 +1,28 @@
+const a = 7
+let b = 3
+
+//atribuição mais adição
+//b = b + a
+b += a
+console.log(b)
+
+//atribuição mais subtração
+//b = b - a
+b -= a
+console.log(b)
+
+//atribuição mais multiplicação
+//b = b * a
+b *= a
+console.log(b)
+
+//atribuição mais divisão
+//b = b / a
+b /= a 
+console.log(b)
+
+//atribuição mais módulo
+//b = b % a
+b %= a
+console.log(b)
+
