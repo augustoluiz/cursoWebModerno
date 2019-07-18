daf3b58be3e705cd8f02ddcab6e4103254fecce9
augustoluiz <augustogu7@yahoo.com.br>
2019-07-17 22:55:59 -0300

    Exercícios de Fundamentos - Operadores Aritméticos

---
+++ exercicios-js/OperadoresAritimeticos.js
@@ -0,0 +1,14 @@
+//Operadores Aritméticos no Js!
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const sub = d - b
+const mult = a * b
+const div = a/b
+const mod = d%a
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(mod)
