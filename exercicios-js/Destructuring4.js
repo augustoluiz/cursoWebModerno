bcdc70c899d06b91557056c1a81c5824defb9890
augustoluiz <augustogu7@yahoo.com.br>
2019-07-17 22:50:37 -0300

    Exercícios de Fundamentos - Destructuring #4

---
+++ exercicios-js/Destructuring4.js
@@ -0,0 +1,12 @@
+//Usando o Destructuring para arrays
+function rand([min = 0, max = 1000]){
+    if(min > max){
+        [min, max] = [max, min]
+    }
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+console.log(rand([50, 40]))
+console.log(rand([4]))
+console.log(rand([,5]))
